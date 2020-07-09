157b0e0f24eedd40220c4d615b24598cfad87a6d
hscottvo <hscottvo@gmail.com>
2020-07-09 12:38:17 -0700

    update proto.py

--- proto.py
+++ proto.py
@@ -86,7 +86,7 @@ def nearest_color(color: str) -> Color:
 Alphanum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-            'u', 'v', 'w', 'x', ' y', 'z']
+            'u', 'v', 'w', 'x', 'y', 'z']
 
 color_dict = {  # Sampled from https://www.auvsi-suas.org/
     Color.White:    '0xfff2f5',
